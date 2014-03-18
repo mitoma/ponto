777dc331abda805017efa589a605ed9124c270ef
Mitoma Ryo <mutetheradio@gmail.com>
2014-03-19 00:03:59 +0900

    サーブレットむずいわ…。

--- src/main/java/in/tombo/ponto/PropertiesService.java
+++ src/main/java/in/tombo/ponto/PropertiesService.java
@@ -1,23 +1,28 @@
 package in.tombo.ponto;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PropertiesService {
-  private Logger logger;
+  private Logger     logger;
 
-  private String[] filePaths;
-  private String envValue;
+  private String[]   filePaths;
+  private String     envValue;
 
   private Properties properties;
   private Properties envProperties;
 
-  public PropertiesService(String loggerName, String envValue, String... filePaths) {
+  public PropertiesService(String loggerName, String envValue,
+      String... filePaths) {
     this.logger = LoggerFactory.getLogger(loggerName);
     this.envValue = envValue;
     this.filePaths = filePaths;
@@ -56,7 +61,8 @@ public class PropertiesService {
           useProp = "default";
           p = properties;
         }
-        logger.info("\tenv:{}\tkey:{}\tvalue:{}", useProp, keyStr, p.get(keyStr));
+        logger.info("\tenv:{}\tkey:{}\tvalue:{}", useProp, keyStr,
+            p.get(keyStr));
       }
     }
   }
@@ -86,7 +92,8 @@ public class PropertiesService {
       }
     } catch (IOException e) {
       logger.error("properties file {} is not found.", filePath);
-      throw new RuntimeException(String.format("properties file %s is not found.", filePath));
+      throw new RuntimeException(String.format(
+          "properties file %s is not found.", filePath));
     }
   }
 
@@ -100,15 +107,35 @@ public class PropertiesService {
   private InputStream getInputStream(String filePath) throws IOException {
     InputStream in;
     if (filePath.startsWith("classpath:")) {
-      in = ClassLoader.getSystemResourceAsStream(filePath.substring(10));
+      in = getInputStreamFromClassPath(filePath.substring(10));
     } else if (filePath.startsWith("file:")) {
-      in = new FileInputStream(filePath.substring(5));
+      in = getInputStreamFromFile(filePath.substring(5));
     } else {
-      in = ClassLoader.getSystemResourceAsStream(filePath);
+      in = getInputStreamFromClassPath(filePath);
     }
     if (in == null) {
       throw new IOException("filePath " + filePath + " is invalid.");
     }
     return in;
+  }
+
+  private FileInputStream getInputStreamFromFile(String filePath)
+      throws FileNotFoundException {
+    return new FileInputStream(filePath);
+  }
+
+  private InputStream getInputStreamFromClassPath(String filePath)
+      throws IOException {
+    Enumeration<URL> resources = Thread.currentThread().getContextClassLoader()
+        .getResources(filePath);
+    for (URL resource : Collections.list(resources)) {
+      logger.info("Resource URL:{}", resource);
+      if ("jar".equals(resource.getProtocol())) {
+        return (InputStream) resource.getContent();
+      } else {
+        return new FileInputStream(resource.getFile());
+      }
+    }
+    return null;
   }
 }
