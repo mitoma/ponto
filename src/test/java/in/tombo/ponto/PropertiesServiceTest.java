5c9bfe37a4358a327d447b18841d704620935d5d
Mitoma Ryo <mutetheradio@gmail.com>
2014-03-18 10:08:22 +0900

    add test.

--- src/test/java/in/tombo/ponto/PropertiesServiceTest.java
+++ src/test/java/in/tombo/ponto/PropertiesServiceTest.java
@@ -8,12 +8,23 @@ import org.junit.Test;
 public class PropertiesServiceTest {
 
   @Test
-  public void testPropertiesService() {
+  public void useEnvProperty() {
     PropertiesService pService = new PropertiesService("hoge.moge", "dev",
         "test1.properties");
     assertThat(pService.getProperties("test.key"), is("devString"));
     assertThat(pService.getProperties("test.intkey.valid._int"), is("1234"));
     assertThat(pService.getProperties("test.longkey.valid._long"), is("23456"));
+    assertThat(pService.getProperties("test.floatkey.valid._float"),
+        is("1234.1234"));
+  }
+
+  @Test
+  public void useInvalidEnvProperty() {
+    PropertiesService pService = new PropertiesService("hoge.moge", "devo",
+        "test1.properties");
+    assertThat(pService.getProperties("test.key"), is("String"));
+    assertThat(pService.getProperties("test.intkey.valid._int"), is("1234"));
+    assertThat(pService.getProperties("test.longkey.valid._long"), is("12345"));
     assertThat(pService.getProperties("test.floatkey.valid._float"),
         is("1234.1234"));
   }
